b27fddf441b6ad002722c62cb644d497ed90b13a
priscian <priscian@gmail.com>
2016-05-09 21:00:35 -0400

    Added function 'cards()'.

--- R/data-frame.R
+++ R/data-frame.R
@@ -398,3 +398,48 @@ insert_row.data.frame <- function(x, newrow, rnum) {
 
   return (x)
 }
+
+
+## Simplify 'read.table()' from 'textConnection()'s; like SAS's 'CARDS' statement.
+#' @export
+cards <- function(x, header=TRUE, as.is=TRUE, check.names=FALSE, stringsAsFactors=FALSE, ...)
+{
+  zz <- textConnection(x)
+  tab <- read.table(zz, header=header, as.is=as.is, check.names=check.names, stringsAsFactors=stringsAsFactors, ...)
+  close(zz)
+
+  return (tab)
+}
+
+## usage:
+# x <- cards('
+# A B C val
+# 1 1 1 10
+# 1 1 1 19
+# 1 1 2 21
+# 1 1 2 28
+# 1 1 2 33
+# 1 2 1 38
+# 1 2 1 46
+# 1 2 1 46
+# 1 2 2 51
+# 1 2 2 56
+# 1 3 1 64
+# 1 3 1 71
+# 1 3 1 77
+# 1 3 2 78
+# 1 3 2 82
+# 2 1 1 88
+# 2 1 1 98
+# 2 1 2 101
+# 2 1 2 104
+# 2 2 1 107
+# 2 2 1 113
+# 2 2 2 118
+# 2 2 2 127
+# 2 3 1 130
+# 2 3 1 130
+# 2 3 2 142
+# 2 3 2 144
+# 2 3 2 155
+# ')
